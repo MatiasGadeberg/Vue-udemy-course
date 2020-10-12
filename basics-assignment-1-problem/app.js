0cb075881880cec243fc033852a1923147f7c584
BMSG (Matias Schaff Gadeberg) <BMSG@nne.com>
2020-10-12 08:06:43 +0200

    Solved first assignment

---
+++ basics-assignment-1-problem/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            name: 'Matias Schaff Gadeberg',
+            age: 29,
+            pictureLink: 'https://spaceplace.nasa.gov/review/gallery-space/CygnusLoop_L.en.jpg',
+        };
+    },
+});
+
+app.mount('#assignment')
