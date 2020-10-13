c9121f405b2e9b7c1dd649c6addf24779b941132
HMGD (Matias Schaff Gadeberg) <hmgd@novonordisk.com>
2020-10-13 20:28:27 +0200

    Finished assigment with components and component calling

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<active-user :name="activeName" :age="activeAge"></active-user>
+		<user-data @update-name="updateName" @update-age="updateAge"></user-data>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				activeName: '',
+				activeAge: ''
+			};
+		},
+		methods: {
+			updateName(name) {
+				this.activeName = name;
+			},
+			updateAge(age) {
+				this.activeAge = age;
+			}
+		}
+	};
+</script>
+
+<style>
+	section {
+		margin: 2rem auto;
+		max-width: 40rem;
+		border-radius: 12px;
+		border: 1px solid #ccc;
+	}
+</style>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,22 @@
+<template>
+	<section>
+		<label>Name</label>
+		<input type="text" v-model="name" @input="updateName" />
+		<label>Age</label>
+		<input type="text" v-model="age" @input="updateAge" />
+	</section>
+</template>
+
+<script>
+	export default {
+		emits: ['update-name', 'update-age'],
+		methods: {
+			updateName() {
+				this.$emit('update-name', this.name);
+			},
+			updateAge() {
+				this.$emit('update-age', this.age);
+			}
+		}
+	};
+</script>
